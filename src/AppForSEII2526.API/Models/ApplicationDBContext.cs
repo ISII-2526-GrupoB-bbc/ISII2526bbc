bc3b749a8f74aa0d33c0a725eef2ff104696f76a
chemaa14 <josemaria.cascales@alu.uclm.es>
2025-09-26 18:41:26 +0200

    En carpeta models he crado el fichero ApplicationDBContext.cs

---
+++ src/AppForSEII2526.API/Models/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppForMovies.API.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+                : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+
+        }
+    }
+}
