c562cfa78114ab128dd592de18f5ee640d5879b1
chemaa14 <josemaria.cascales@alu.uclm.es>
2025-09-29 20:10:29 +0200

    Clase Purchase (#14)
    * Clase model finalizada
    
    * Creacion de la clase Purchase

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.Models
+{
+    public class Purchase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(50)]
+        public string Surname { get; set; }
+
+        [Required, StringLength(50)]
+        public string DeliveryCarDealer { get; set; }
+
+        [Required]
+        public string PaymentMethod { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PurchasingDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal PurchasingPrice { get; set; }
+    }
+}
