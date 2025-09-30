fa287d87b292d73e2bc914e4a1531bb723f60922
Isra393 <127391461+Isra393@users.noreply.github.com>
2025-09-30 17:05:27 +0200

    Clase Review (#18)
    * Clase review

---
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Review
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required, StringLength(50)]
+    public string UserName { get; set; }
+
+    [Required, StringLength(50)]
+    public string Country { get; set; }
+
+    [Required]
+    public string DriverType { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime Created { get; set; }
+
+}
