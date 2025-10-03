f30aa763d180b6489cda4af2c076a74424695285
Gaspar Martinez Rubio <gasparmartinezrubio@gmail.com>
2025-10-03 10:33:20 +0200

    He modificado la clase ApplicationUser con los atributos Name y Surname

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,9 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    [Key, StringLength(30)]
+    public string Name { get; set; }
+
+    [Required, StringLength(30)]
+    public string Surname { get; set; }
 }
