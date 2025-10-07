2de6066d680c37402e18b03b3507dd1e6ccbb213
Isra393 <127391461+Isra393@users.noreply.github.com>
2025-10-07 18:56:35 +0200

    Modificar la clase Car porque daba error (#64)

--- src/AppForSEII2526.API/Models/Car.cs
+++ src/AppForSEII2526.API/Models/Car.cs
@@ -1,26 +1,29 @@
-public class Car
+namespace AppForSEII2526.Models
 {
-    [Key]
-    public int Id { get; set; }
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
 
-    [Required, StringLength(30)]
-    public string CarClass { get; set; }
+        [Required, StringLength(30)]
+        public string CarClass { get; set; }
 
-    [Required, StringLength(50)]
-    public string Description { get; set; }
+        [Required, StringLength(50)]
+        public string Description { get; set; }
 
-    [Required, StringLength(30)]
-    public string Manufacturer { get; set; }
+        [Required, StringLength(30)]
+        public string Manufacturer { get; set; }
 
-    public int PurchasingPrice { get; set; }
-    public int QuantityForPurchasing { get; set; }
-    public int QuantityForRenting { get; set; }
-    public int RentalIntems { get; set; }
-    public int RentingPrice { get; set; }
-    public int ReviewItems { get; set; }
+        public int PurchasingPrice { get; set; }
+        public int QuantityForPurchasing { get; set; }
+        public int QuantityForRenting { get; set; }
+    
+        public int RentingPrice { get; set; }
+       
 
-    public Model Model { get; set; }
-    public IList<RentalItem> RentalItems { get; set; }
-    public IList<PurchaseItem> PurchaseItems { get; set; }
-    public IList<ReviewItem> ReviewItems { get; set; }
+        public Model Model { get; set; }
+        public IList<RentalItem> RentalItems { get; set; }
+        public IList<PurchaseItem> PurchaseItems { get; set; }
+        public IList<ReviewItem> ReviewItems { get; set; }
+    }
 }
