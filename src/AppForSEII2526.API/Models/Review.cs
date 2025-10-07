b31aa09342ea443263e199178bfa956a5cccd777
Israel <Israel.Amador@alu.uclm.es>
2025-10-07 18:49:05 +0200

    Modicar la clase review para evitar errores

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -1,21 +1,23 @@
 ﻿using System;
-
-public class Review
+namespace AppForSEII2526.Models
 {
-    [Key]
-    public int Id { get; set; }
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
 
-    [Required, StringLength(50)]
-    public string UserName { get; set; }
+        [Required, StringLength(50)]
+        public string UserName { get; set; }
 
-    [Required, StringLength(50)]
-    public string Country { get; set; }
+        [Required, StringLength(50)]
+        public string Country { get; set; }
 
-    [Required]
-    public string DriverType { get; set; }
+        [Required]
+        public string DriverType { get; set; }
 
-    [DataType(DataType.Date)]
-    public DateTime Created { get; set; }
-    public IList<ReviewItem> ReviewItems { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; }
+        public IList<ReviewItem> ReviewItems { get; set; }
 
+    }
 }
