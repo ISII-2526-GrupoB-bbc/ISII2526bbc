5a59ec2394904a4802851883e4d73c92aefba9be
chemaa14 <josemaria.cascales@alu.uclm.es>
2025-10-17 23:01:23 +0200

    He realizado el controlador para comprar un coche con su metodo get + select, que filtra por el color del coche. (#116)
    Tambien la clase CocheParaComprarDTO utilizada en el controlador

---
+++ src/AppForSEII2526.API/Controllers/CarsControllers.cs
@@ -0,0 +1,65 @@
+﻿using AppForSEII2526.API.DTOs.CochesDTO;
+using AppForSEII2526.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsControllers : ControllerBase
+    {
+        //Un controlador para la base de datos
+        private readonly ApplicationDbContext _context;
+        //Un log de problemas
+        private readonly ILogger<CarsControllers> _logger;
+
+        public CarsControllers(ApplicationDbContext context, ILogger<CarsControllers> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IEnumerable<CocheParaComprarDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<IEnumerable<CocheParaComprarDTO>>> GetCars(string? color = null)
+        {
+            try
+            {
+                var query = _context.Cars.AsQueryable();
+
+                // Aplicar filtro SOLO por color (flujo alternativo)
+                if (!string.IsNullOrEmpty(color))
+                    query = query.Where(c => c.Color.Contains(color));
+
+                var cars = await query
+                    .Select(c => new CocheParaComprarDTO(
+                        c.Id,
+                        c.Model.Name,
+                        c.Color,
+                        c.CarClass,
+                        c.Manufacturer,
+                        c.PurchasingPrice
+                    ))
+                    .ToListAsync();
+
+                if (cars == null || !cars.Any())
+                    return NotFound("No se encontraron coches con ese color.");
+
+                return Ok(cars);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Error en GetCars() - {ex.Message}");
+                return BadRequest("Error al obtener los coches disponibles.");
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/CochesDTO/CocheParaComprarDTO.cs
@@ -0,0 +1,52 @@
+﻿using AppForSEII2526.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Drawing;
+using System.Formats.Asn1;
+
+namespace AppForSEII2526.API.DTOs.CochesDTO
+{
+    public class CocheParaComprarDTO
+    {
+        // Constructor con parámetros
+        public CocheParaComprarDTO(int id,string modelName,string color,string fuelType,string manufacturer ,decimal purchasingPrice)
+        {
+            Id = id;
+            ModelName = modelName;
+            Color = color;
+            FuelType = fuelType; 
+            Manufacturer = manufacturer;
+            PurchasingPrice = purchasingPrice;
+        }
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El modelo del coche es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre del modelo no puede tener más de 50 caracteres.")]
+        [Display(Name = "Modelo del coche")]
+        public string ModelName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El color es obligatorio.")]
+        [StringLength(30, ErrorMessage = "El color no puede tener más de 30 caracteres.")]
+        [Display(Name = "Color del coche")]
+        public string Color { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El tipo de gasoil es obligatorio")]
+        [Display(Name = "Tipo de gasoil ")]
+        public string FuelType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El fabricante es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre del fabricante no puede tener más de 50 caracteres.")]
+        [Display(Name = "Fabricante")]
+        public string Manufacturer { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El precio de compra es obligatorio.")]
+        [Range(0, 200000, ErrorMessage = "El precio debe ser un valor positivo.")]
+        [Display(Name = "Precio de compra (€)")]
+        public decimal PurchasingPrice { get; set; }
+
+
+
+
+    }
+}
