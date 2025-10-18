c69d765b36ac52c4e41eea834a9c1994267b5f4c
Isra393 <127391461+Isra393@users.noreply.github.com>
2025-10-18 18:27:25 +0200

    Controlador del caso de uso Review (#118)

---
+++ src/AppForSEII2526.API/Controllers/CarsController.cs
@@ -0,0 +1,58 @@
+﻿using AppForSEII2526.API.DTOs.CochesDTO;
+using AppForSEII2526.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        // used to enable your controllers to access the database
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ApplicationDbContext context, ILogger<CarsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IEnumerable<CocheParaReviewDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<IEnumerable<CocheParaReviewDTO>>> GetCars(string? FuelType = null)
+        {
+            try
+            {
+                var query = _context.Cars.AsQueryable();
+
+                // Aplicar filtro SOLO por color (flujo alternativo)
+                if (!string.IsNullOrEmpty(FuelType))
+                    query = query.Where(c => c.FuelType.Contains(FuelType));
+
+                var cars = await query
+                    .Select(c => new CocheParaReviewDTO(
+                        c.Id,
+                        c.Model.Name,
+                        c.Color,
+                        c.CarClass,
+                        c.Manufacturer,
+                        c.FuelType
+                    ))
+                    .ToListAsync();
+
+                if (cars == null || !cars.Any())
+                    return NotFound("No se encontraron coches con ese tipo de combustible.");
+
+                return Ok(cars);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Error en GetCars() - {ex.Message}");
+                return BadRequest("Error al obtener los coches disponibles.");
+            }
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/CochesDTO/CocheParaReviewDTO.cs
@@ -0,0 +1,50 @@
+﻿using AppForSEII2526.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Drawing;
+using System.Formats.Asn1;
+
+namespace AppForSEII2526.API.DTOs.CochesDTO
+{
+    public class CocheParaReviewDTO
+    {
+        // Constructor con parámetros
+        public CocheParaReviewDTO(int id, string modelName, string color, string fuelType, string manufacturer, string carclass)
+        {
+            Id = id;
+            ModelName = modelName;
+            Color = color;
+            FuelType = fuelType;
+            Manufacturer = manufacturer;
+            CarClass = carclass;
+        }
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El modelo del coche es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre del modelo no puede tener más de 50 caracteres.")]
+        [Display(Name = "Modelo del coche")]
+        public string ModelName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El color es obligatorio.")]
+        [StringLength(30, ErrorMessage = "El color no puede tener más de 30 caracteres.")]
+        [Display(Name = "Color del coche")]
+        public string Color { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El tipo de gasoil es obligatorio")]
+        [Display(Name = "Tipo de gasoil ")]
+        public string FuelType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El fabricante es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre del fabricante no puede tener más de 50 caracteres.")]
+        [Display(Name = "Fabricante")]
+        public string Manufacturer { get; set; } = string.Empty;
+
+        [Required, StringLength(30)]
+        public string CarClass { get; set; }
+
+
+
+
+    }
+}
