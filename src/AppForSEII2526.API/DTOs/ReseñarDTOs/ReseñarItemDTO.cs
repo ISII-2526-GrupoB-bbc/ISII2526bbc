17b87a738f2a7532676adaebd559f61aa2cd43b9
Isra393 <127391461+Isra393@users.noreply.github.com>
2025-10-24 21:10:04 +0200

    He creado el DTO ReseñarItemDTO (#135)

---
+++ src/AppForSEII2526.API/DTOs/ReseñarDTOs/ReseñarItemDTO.cs
@@ -0,0 +1,34 @@
+﻿namespace AppForSEII2526.API.DTOs.ReseñarDTOs
+{
+    public class ReseñarItemDTO
+    {
+        public ReseñarItemDTO(string modelo, string manufacturer, string color, int rating, string description = "")
+        {
+            Modelo = modelo;
+            Manufacturer = manufacturer;
+            Color = color;
+            Rating = rating;
+            Description = description;
+        }
+        public string Modelo { get; set; }
+        public string Manufacturer { get; set; }
+        public string Color { get; set; }
+        public int Rating { get; set; }
+        public string? Description { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReseñarItemDTO dTO &&
+                   Modelo == dTO.Modelo &&
+                   Manufacturer == dTO.Manufacturer &&
+                   Color == dTO.Color &&
+                   Rating == dTO.Rating &&
+                   Description == dTO.Description;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Modelo, Manufacturer, Color, Rating, Description);
+
+        }
+    }
+}
