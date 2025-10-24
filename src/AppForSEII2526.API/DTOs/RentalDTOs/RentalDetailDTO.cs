0558b3de9231e302d258eba203ffaefde16e0320
Gaspar Martinez Rubio <gasparmartinezrubio@gmail.com>
2025-10-24 20:15:08 +0200

    He creado RentalDetailDTO

---
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalDetailDTO.cs
@@ -0,0 +1,45 @@
+﻿namespace AppForMovies.API.DTOs.RentalDTOs
+{
+    public class RentalDetailDTO : RentalForCreateDTO
+    {
+        public decimal TotalPrice { get; set; }
+
+        public RentalDetailDTO(
+            int id,
+            string customerName,
+            string customerSurname,
+            string deliveryAddress,
+            Rental.PaymentMethod paymentMethod,
+            DateTime rentalDateFrom,
+            DateTime rentalDateTo,
+            DateTime rentalDate,
+            decimal totalPrice,
+            IList<RentalItemDTO> rentalItems
+        ) : base(customerName, customerSurname, deliveryAddress, paymentMethod, rentalDateFrom, rentalDateTo, rentalItems)
+        {
+            Id = id;
+            RentalDate = rentalDate;
+            TotalPrice = totalPrice;
+        }
+        public int Id { get; set; }
+
+        public DateTime RentalDate { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is RentalDetailDTO dto)
+            {
+                return base.Equals(obj) &&
+                   TotalPrice == dto.TotalPrice &&
+                   Id == dto.Id &&
+                   RentalDate.Date == dto.RentalDate.Date;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Id, RentalDate);
+        }
+    }
+}
