f7c9215d4977d7d73e3fda12bcc5a7ab4fef4fac
Gaspar Martinez Rubio <gasparmartinezrubio@gmail.com>
2025-10-25 09:30:00 +0200

    He creado RentalItemDTO

---
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -0,0 +1,40 @@
+﻿using AppForSEII2526.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.DTOs.CochesDTO
+{
+    public class RentalItemDTO
+    {
+        public RentalItemDTO(int id, Model model, string manufacturer, decimal rentalPrice, int quantity)
+        {
+            Id = id;
+            Model = model;
+            Manufacturer = manufacturer;
+            TotalPrice = rentalPrice;
+            Quantity = quantity;
+        }
+
+
+        public int Id { get; set; }
+        public Model Model { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RentalItemDTO dto &&
+                   Id == dto.Id &&
+                   Equals(Model, dto.Model) &&
+                   Manufacturer == dto.Manufacturer &&
+                   TotalPrice == dto.TotalPrice &&
+                   Quantity == dto.Quantity;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Model, Manufacturer, TotalPrice, Quantity);
+        }
+    }
+}
