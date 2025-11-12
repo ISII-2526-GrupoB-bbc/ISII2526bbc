f9ebfc0808977a131c7ff1f32f6c9a9a2033fe31
chemaa14 <josemaria.cascales@alu.uclm.es>
2025-11-12 17:51:12 +0100

    solucion errores metodo detail para comprar coche (#183)
    * Añadido el SeedData, modificacion en ApplicationUser y Program.cs
    
    * solucion errores para el metodo detail de comprar un coche

--- src/AppForSEII2526.API/Models/Car.cs
+++ src/AppForSEII2526.API/Models/Car.cs
@@ -20,19 +20,19 @@ namespace AppForSEII2526.Models
         [Required, StringLength(30)]
         public string Manufacturer { get; set; }
 
-        [Range(0, int.MaxValue, ErrorMessage = "El precio de compra no puede ser negativo.")]
-        public int PurchasingPrice { get; set; }
+        [Range(typeof(decimal), "0", "2000000", ErrorMessage = "El precio de compra no puede ser negativo.")]
+        public decimal PurchasingPrice { get; set; }
 
-        [Range(0, int.MaxValue, ErrorMessage = "La cantidad para compra no puede ser negativa.")]
-        public int QuantityForPurchasing { get; set; }
+        [Range(typeof(decimal), "0", "20000000", ErrorMessage = "La cantidad para compra no puede ser negativa.")]
+        public decimal QuantityForPurchasing { get; set; }
 
-        [Range(0, int.MaxValue, ErrorMessage = "La cantidad para alquiler no puede ser negativa.")]
-        public int QuantityForRenting { get; set; }
+        [Range(typeof(decimal), "0", "20000000", ErrorMessage = "La cantidad para alquiler no puede ser negativa.")]
+        public decimal QuantityForRenting { get; set; }
 
         [Range(0, int.MaxValue, ErrorMessage = "El precio de alquiler no puede ser negativo.")]
         public decimal RentingPrice { get; set; }
-       
 
+        [JsonIgnore] // Evita el bucle infinito al serializar
         public Model Model { get; set; }
         public IList<RentalItem> RentalItems { get; set; }
         public IList<PurchaseItem> PurchaseItems { get; set; }
