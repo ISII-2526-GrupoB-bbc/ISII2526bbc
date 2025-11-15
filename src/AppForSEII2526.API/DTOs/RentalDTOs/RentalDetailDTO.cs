4126df4a6395ce1537a802f421a7210951384201
Gaspaar11uclm <gaspar.martinez1@alu.uclm.es>
2025-11-15 17:48:20 +0100

    Cmabios en los DTOs y RentalsController, había que modificarlos para que funcione bien (#190)
    Co-authored-by: Gaspar Martinez Rubio <gasparmartinezrubio@gmail.com>

--- src/AppForSEII2526.API/DTOs/RentalDTOs/RentalDetailDTO.cs
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalDetailDTO.cs
@@ -2,63 +2,59 @@
 
 namespace AppForSEII2526.API.DTOs.RentalDTOs
 {
-    public class RentalDetailDTO : RentalForCreateDTO
+    public class RentalDetailDTO
     {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string DeliveryCarDealer { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime RentingDate { get; set; }
         public decimal RentingPrice { get; set; }
+        public IList<RentalItemDTO> RentalItems { get; set; }
 
         public RentalDetailDTO(
             int id,
             string customerName,
             string customerSurname,
-            string deliveryAddress,
+            string deliveryCarDealer,
             PaymentMethod paymentMethod,
-            DateTime rentalDateFrom,
-            DateTime rentalDateTo,
-            DateTime rentalDate,
+            DateTime startDate,
+            DateTime endDate,
+            DateTime rentingDate,
             decimal rentingPrice,
             IList<RentalItemDTO> rentalItems
-        ) : base(customerName, customerSurname, deliveryAddress, paymentMethod, rentalDateFrom, rentalDateTo, rentalItems)
+        )
         {
             Id = id;
-            RentalDate = rentalDate;
-            RentingPrice = rentingPrice;
-        }
-
-        public RentalDetailDTO(string name, string surname, string deliveryCarDealer, PaymentMethod paymentMethod, DateTime startDate, DateTime endDate, DateTime rentingDate, decimal rentingPrice, List<RentalItemDTO> rentalItemDTOs)
-        {
-            Name = name;
-            Surname = surname;
+            CustomerName = customerName;
+            CustomerSurname = customerSurname;
             DeliveryCarDealer = deliveryCarDealer;
             PaymentMethod = paymentMethod;
             StartDate = startDate;
             EndDate = endDate;
             RentingDate = rentingDate;
             RentingPrice = rentingPrice;
-            RentalItemDTOs = rentalItemDTOs;
+            RentalItems = rentalItems;
         }
-
-        public int Id { get; set; }
-
-        public DateTime RentalDate { get; set; }
-        public string DeliveryCarDealer { get; }
-        public DateTime RentingDate { get; }
-        public List<RentalItemDTO> RentalItemDTOs { get; }
 
         public override bool Equals(object? obj)
         {
-            if (obj is RentalDetailDTO dto)
-            {
-                return base.Equals(obj) &&
-                   RentingPrice == dto.RentingPrice &&
-                   Id == dto.Id &&
-                   RentalDate.Date == dto.RentalDate.Date;
-            }
-            return false;
-        }
+            if (obj is not RentalDetailDTO other)
+                return false;
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(base.GetHashCode(), Id, RentalDate);
+            return Id == other.Id &&
+                   CustomerName == other.CustomerName &&
+                   CustomerSurname == other.CustomerSurname &&
+                   DeliveryCarDealer == other.DeliveryCarDealer &&
+                   PaymentMethod == other.PaymentMethod &&
+                   StartDate.Date == other.StartDate.Date &&
+                   EndDate.Date == other.EndDate.Date &&
+                   RentingDate.Date == other.RentingDate.Date &&
+                   RentingPrice == other.RentingPrice &&
+                   RentalItems.SequenceEqual(other.RentalItems);
         }
     }
 }
--- src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -1,40 +1,37 @@
-﻿using AppForSEII2526.Models;
-using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace AppForSEII2526.API.DTOs
+﻿public class RentalItemDTO
 {
-    public class RentalItemDTO
+    public int Id { get; set; }
+    public int ModelId { get; set; }
+    public string ModelName { get; set; }
+    public string Manufacturer { get; set; }
+    public decimal RentingPrice { get; set; }
+    public int Quantity { get; set; }
+
+    public RentalItemDTO() { }
+
+    public RentalItemDTO(int id, int modelId, string modelName, string manufacturer, decimal rentingPrice, int quantity)
     {
-        public RentalItemDTO(int id, Model model, string manufacturer, decimal rentalPrice, int quantity)
-        {
-            Id = id;
-            Model = model;
-            Manufacturer = manufacturer;
-            RentingPrice = rentalPrice;
-            Quantity = quantity;
-        }
+        Id = id;
+        ModelId = modelId;
+        ModelName = modelName;
+        Manufacturer = manufacturer;
+        RentingPrice = rentingPrice;
+        Quantity = quantity;
+    }
 
+    public override bool Equals(object? obj)
+    {
+        return obj is RentalItemDTO dto &&
+               Id == dto.Id &&
+               ModelId == dto.ModelId &&
+               ModelName == dto.ModelName &&
+               Manufacturer == dto.Manufacturer &&
+               RentingPrice == dto.RentingPrice &&
+               Quantity == dto.Quantity;
+    }
 
-        public int Id { get; set; }
-        public Model Model { get; set; }
-        public string Manufacturer { get; set; }
-        public decimal RentingPrice { get; set; }
-        public int Quantity { get; set; }
-
-        public override bool Equals(object? obj)
-        {
-            return obj is RentalItemDTO dto &&
-                   Id == dto.Id &&
-                   Equals(Model, dto.Model) &&
-                   Manufacturer == dto.Manufacturer &&
-                   RentingPrice == dto.RentingPrice &&
-                   Quantity == dto.Quantity;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, Model, Manufacturer, RentingPrice, Quantity);
-        }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, ModelId, ModelName, Manufacturer, RentingPrice, Quantity);
     }
 }
