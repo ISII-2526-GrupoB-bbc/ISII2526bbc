525b18a54e54f9a03ef472057fe778bc505f8024
chemaa14 <josemaria.cascales@alu.uclm.es>
2025-11-18 13:45:51 +0100

    Pruebas Unitarias del post  + Comentarios del post. Para el caso de uso Comprar Coche

---
+++ test/AppForSEII2526.UT/CarsController_test/PurchaseControllerTest/PostComprarCoche_test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UT.CarsController_test.PurchaseControllerTest
+{
+    internal class PostComprarCoche_test
+    {
+    }
+}
